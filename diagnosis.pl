e01e5c2be6e1e98c0dc390cf0b547a7d97e9e99f
MarcoServinDLT <71104348+MarcoServinDLT@users.noreply.github.com>
2021-11-18 18:54:12 -0600

    Cambios de 18/11/2021
    Se añadieron procedimientos al sistema para la evaluación de criterios con múltiples síntomas y evaluación de los criterios de todo un trastorno, además de agregar la primer base de conocimientos definida por defecto.

--- diagnosis.pl
+++ diagnosis.pl
@@ -1,11 +1,22 @@
-symptom(Symptom) :- 
-    write('El paciente presenta '), write(Symptom), nl,
-    read(Respose),
-    Respose == 'Si' -> true ; false.
+% -- Caso base del procedimiento para preguntar todos los sintomas -- %
+symptom([]) :- !.
 
+% -- Rcorrido de todas la preguntas referentes a los sintomas del trastorno -- %
+symptom([H|T]) :-
+    (symptom(H), symptom(T), !) ;
+    fail, !.
+
+% -- Pregunstas individuales de sintomsa -- %
+symptom(Symptom) :- 
+    write(Symptom), nl,
+    read(Respose),
+    Respose == 'Si'.
+
+% -- Caso base del procedimietno para calcular el cumplimiento de sintomas múltiples -- %
 multiSympton([], Min, _, Matches) :-
     Min =< Matches.
 
+% -- Procedimiento para criterios de sintomas múltiples -- %
 multiSympton([H|T], Min, Symptoms, Matches) :-
     S is Symptoms + 1,
     symptom(H) -> M is Matches + 1, multiSympton(T, Min, S, M);
@@ -13,20 +24,22 @@ multiSympton([H|T], Min, Symptoms, Matches) :-
     Min - Matches =< L, 
     multiSympton(T, Min, Symptoms, Matches).
 
-phobia(_, 
-    ['publico', 'cerrado', 'abiertos', 'multitud', 'solo']).
+% -- Procedimiento para alamcenar las respuestas del paciente -- %
+:- dynamic patientAnswers/2.
 
-depresion(_,
-    ['deprimido','peso','sueño','inutilidad','muerte']).
+% -- Procedimiento para agregar una respuesta -- %
+addAnswer(Question, Asnwer) :-
+    \+ patientAnswers(Question, _),
+    assert(patientAnswers(Question, Asnwer)).
 
-ansiedad(_,
-    'preocupacion','tension','irritabilidad','fatigarse','atrapado').
+% -- Procedimiento para alimentar la base de conocimientos del sistema -- %
+%addJudment(Disorder) :-
+    %forall(patientAnswers(Question, 'si'), ),
+    %assert(disorders(Disorder, Questions)).
 
-agorafobia(_,
-    'multitud','trasporte','miedo','ansiedad','solo').
-
+% -- Procedimiento para realizar un diagnóstico -- %
 diagnosis :-
-    phobia(_,M),
-    multiSympton(M, 2, 0, 0). 
-
-
+    consult("knowledge.pl"),
+    disorders("Transtorno de Fobia Especifica", L),
+    length(L, Tam),
+    multiSympton(L, Tam, 0, 0).
---
+++ knowledge.pl
@@ -0,0 +1,79 @@
+:- dynamic disorders/2.
+
+disorders("Transtorno de Ansiedad Generalizada", 
+[
+    [
+        "El paciente presenta problemas emocionales como miedo o ansiedad",
+        "Dichos problemas han tenido consecuencias negativas en entornos importantes del paciente como lo son el entorno social y laboral"
+    ],
+    [
+        "El paciente ha tenido episodios de ansiedad",
+        "Dichos episodios han estado vigentes en periodos prolongados de 6 meses o mas"    
+    ],
+    [
+        "El paciente tiene una inquietud constante provocada por la sensacion de estar atrapado",
+        "El paciente se fatiga con facilidad",
+        "El concentrarse resulta ser una tarea dificil para el paciente",
+        "El paciente se molesta con facilidad",
+        "El paciente tiene dificultades para conciliar el sueño o tener una calidad de sueño satisfactoria"    
+    ]
+]
+).
+
+disorders("Transtorno de Fobia Especifica", 
+[
+    [
+        "El paciente presenta problemas emocionales como miedo o ansiedad",
+        "Dichos problemas han tenido consecuencias negativas en entornos importantes del paciente como lo son el entorno social y laboral"
+    ],
+    [
+        "El paciente ha tenido episodios de ansiedad",
+        "Dichos episodios han estado vigentes en periodos prolongados de 6 meses o mas"    
+    ],
+    "La ansiedad que el paciente presenta se puede atribuir a una situacion u objeto en especial",
+    "El paciente llora al estar envuelto en dicha situacion o al observar el objeto en particular",
+    "El paciente evita o opone resistencia activamente a los eventos que le causan ansiedad",
+    "La ansiedad que el paciente presenta es desproporcionada al peligro que podria suponer la situacion o el objeto",
+    "El paciente ha sufrido alguna experiencia negativa en la que un evento similar haya ocurrido Sin tomar en cuenta los ataques de ansiedad",
+    "El paciente presenta ataques de ansiedad cuando debe realizar actividades fuera de casa"
+]
+).
+
+disorders("Agorafobia",
+[
+    "En dichas situaciones El paciente se encuentra en un estado de alerta constante en el cual los pensamientos recurrentes estan relacionados con lo dificil que seria recibir ayuda en caso de una tragedia",
+    [
+        "El usar el transporte publico causa ansiedad al paciente",
+        "El paciente teme a los espacios abiertos",
+        "El paciente teme a los sitios cerrados",
+        "El paciente sufre ansiedad cuando esta rodeado de muchas personas",
+        "Para el paciente es imprescindible la compañia de algun conocido para realizar actividades fuera de casa"
+    ],
+    "El paciente presenta afecciones patologicas como problemas inflamatorios gastrointestinales o neurologicos como parkinson derivados de episodios de ansiedad",
+    "El estado animico del paciente no puede ser atribuido al estado de salud fisiologico ya sea por enfermedad o por uso  de sustancias",
+    "El paciente esta en un estado constante de preocupacion"
+]
+).
+
+disorders("Transtorno Depresivo Mayor",
+[
+    "El estado animico del paciente no puede ser atribuido al estado de salud fisiologico ya sea por enfermedad o por uso  de sustancias",
+    [
+        "El paciente presenta un estado animico mayormente depresivo",
+        "El paciente se molesta con facilidad",
+        "El paciente demuestra una perdida de interes o satisfaccion en la realizacion de sus actividades cotidianas",
+        "El paciente ha experimentado una perdida o aumento de peso alarmante derivado de desordenes alimenticios provocados ya sea por la falta o aumento del apetito", 
+        "El paciente tiene problemas con el sueño llegando a extremos es decir presenta insomnio o hipersomnia.",
+        "El paciente tiene problemas de psicomotricidad notables",
+        "El paciente se fatiga con facilidad derivado de la falta de energia",
+        "El paciente tiene sentimientos negativos hacia su persona, tal como sentimiento de inutilidad o culpabilidad excesiva o inapropiada",
+        "El paciente sufre de dificultad para concentrarse la cual afecta su proceso de toma de decisiones"
+    ],
+    [
+        "El paciente tiene pensamientos relacionados con la muerte frecuentemente no unicamente con relacion al miedo que esta situacion supone",
+        "El paciente ha tenido ideas suicidas"
+    ],
+    "Durante el periodo reciente, el paciente ha presentado episodios de mania o hipomania",
+    "El paciente ha presentado ningun episodio psicotico que NO pueda ser explicado por trastornos del espectro de la esquizofrenia"
+]
+).
