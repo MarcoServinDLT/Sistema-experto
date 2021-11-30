262925706469206df7d217c0e7fd124e4745d7d1
EdgarMartinezEsqueda <71104237+EdgarMartinezEsqueda@users.noreply.github.com>
2021-11-29 23:33:37 -0600

    Agregado el insertar nuevos transtornos
    Agregada ventanas para la visualización del transtorno obtenido o por el contrario el que no se encontró nada, además el usuario puede ingresar el nombre de un nuevo transtorno y las preguntas que tendrá

--- diagnosis.pl
+++ diagnosis.pl
@@ -1,3 +1,4 @@
+:- encoding(utf8).
 % --------------------- Procedimiento para determinar si es lista --------------------- %
 isList([_]) :- !.
 isList([_|_]) :- !.
@@ -35,13 +36,40 @@ addAnswer(Question, Asnwer, Window) :-
     assert(patientAnswers(Question, Asnwer)), 
     send(Window, close).
 
+% --------------------- Procedimiento para alamcenar el transtorno encontrado --------------------- %
+:- dynamic transtorno/1.
+
 % --------------------- Procedimiento para realizar un diagnóstico --------------------- %
-diagnosis(Disorder) :-
+diagnosis(Window) :-
+    retractall(patientAnswers(_,_)),    
     consult("knowledge.pl"),
     forall(disorders(T, L), (
         length(L, Tam),
         multiSympton(L, Tam, 0, 0),
-        writeln(T);
+        assert(transtorno(T))
+        ;
         true
     )),
-    retractall(patientAnswers(_,_)).
+    transtorno(T),
+    send(Window, display, new(Diagnostico, dialog_group('Resultado')), point(500,250)),
+    send(Diagnostico, append, new(Disorder, text_item("Encontramos que usted tiene"))),
+    send(Diagnostico, append, new(button('No estoy de acuerdo', message(@prolog, addLearning)))),
+    send(Disorder, editable, false),
+    send(Disorder, selection, T),
+    retractall(transtorno(_))
+    ;
+    send(Window, display, new(Diagnostico, dialog_group('No encontramos su transtorno')), point(500,250)),
+    send(Diagnostico, append, new(button('Agregar nuevo transtorno', message(@prolog, addLearning)))).
+
+% ---------------------- Procedimiento para agregar un nuevo transtorno -------------------- %
+addLearning :- 
+    consult("learning.pl"),
+    new(NewDisorder, dialog('Agregar nuevo transtorno')),
+    new(NameDisorder, text_item('Ingrese el nombre del transtorno')),
+    send(NewDisorder, append, NameDisorder),
+    send(NewDisorder, append, new(label(instruction,'¿Desea agregar preguntas especificas para este transtorno?'))),
+    send(NewDisorder, append, new(button('Agregar preguntas',  message(@prolog, questionJudment, NameDisorder?selection, NewDisorder)))),
+    send(NewDisorder, append, new(button('Guardar',   message(@prolog, addJudment, NameDisorder?selection, NewDisorder)))),
+    send(NewDisorder, open_centered),
+    send(NewDisorder, open).
+    
